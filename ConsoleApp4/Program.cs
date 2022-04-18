5e2b3d0cc34d15dc8bfe5d6119382ffbd0cdf274
Naila I. Bunyatova <nailaib@code.local>
2022-04-18 18:33:07 +0400

    salamm

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 48;
+            for (int i = 0; i < number; i++)
+            {
+                if (number <= 50)
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                else if (number > 50 && number < 100)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+                else if (number > 100)
+                {
+                    if (i % 8 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+    }
+}
